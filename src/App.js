5f4d2a55cb6ba14caf189c114282f1b085ddd162
Guilherme Moura <gmoura.nas@gmail.com>
2018-11-30 17:51:51 -0200

    feat(style): add some style and laboratoria's logo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-
 import fechGifsFrontServer from './actions';
-
 import Gifs from './components/Gifs';
+import logo from './images/logo.svg';
 
-const App = ({ dispatch, gifs }) => (
-  <React.Fragment>
-    <Typography variant="h1" align="center" gutterBottom>
-      <FormattedMessage id="hello.world" />
-    </Typography>
-    <Button
-      variant="contained"
-      color="primary"
-      onClick={() => dispatch(fechGifsFrontServer)}
-    >
-      <FormattedMessage id="load.gifs" />
-    </Button>
-    <Gifs data={gifs} />
-  </React.Fragment>
+const styles = theme => ({
+  root: {
+    flexFlow: 1,
+    textAlign: 'center'
+  },
+  logo: {
+    padding: 15,
+    borderBottom: 'solid 1px',
+    borderColor: theme.palette.primary.dark
+  },
+  button: {
+    margin: '20px 0',
+    padding: '0 0 20px 0'
+  }
+});
+
+const App = ({ classes, dispatch, gifs }) => (
+  <Grid container justify="center" className={classes.root}>
+    <Grid item xs={12} className={classes.logo}>
+      <img src={logo} alt="Laboratoria" />
+    </Grid>
+    <Grid item xs={12}>
+      <Typography variant="h1" align="center" gutterBottom>
+        <FormattedMessage id="hello.world" />
+      </Typography>
+    </Grid>
+
+    <Grid item xs={12} className={classes.button}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => dispatch(fechGifsFrontServer)}
+      >
+        <FormattedMessage id="load.gifs" />
+      </Button>
+    </Grid>
+
+    <Grid item xs={12}>
+      <Gifs data={gifs} />
+    </Grid>
+  </Grid>
 );
 
 const mapStateToProps = state => ({
   gifs: state.gifs
 });
 
-export default connect(mapStateToProps)(App);
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps)
+)(App);
--- src/components/Gifs.js
+++ src/components/Gifs.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
-const Gifs = ({ data }) => (
-  <React.Fragment>
-    {data.length > 0 &&
-      data.map(gif => (
-        <img src={gif.images.downsized_medium.url} alt={gif.title} />
-      ))}
-  </React.Fragment>
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper
+  },
+  gridList: {
+    width: 500,
+    height: 450
+  },
+  subheader: {
+    width: '100%'
+  }
+});
+
+const Gifs = ({ classes, data }) => (
+  <div className={classes.root}>
+    <GridList cellHeight={160} className={classes.gridList} cols={3}>
+      {data.length > 0 &&
+        data.map(gif => (
+          <GridListTile key={gif.title} cols={1}>
+            <img src={gif.images.downsized_medium.url} alt={gif.title} />
+          </GridListTile>
+        ))}
+    </GridList>
+  </div>
 );
 
-export default Gifs;
+export default withStyles(styles)(Gifs);
